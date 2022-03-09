2d581d124bbda709a322c76f549d58c34d4bdb81
KipshidzeNikoloz <kipshidzenikoloz@gmail.com>
2022-03-10 02:19:57 +0400

    Added full JS functionality, improved styling, TBD refactor

--- script.js
+++ script.js
@@ -1,25 +1,116 @@
-let myLibrary = []
+//Query Selectors + Event Listeners 
 
-function Book(title, author, pages, read) {
-    this.title = title
-    this.author = author
-    this.pages = pages
-    this.read = read
-    this.info = function() {
-        return this.title + " by " + this.author + ', ' + this.pages + ' pages, ' + this.read
+const addBtn = document.querySelector('.addNew');
+const submit = document.querySelector('.submit');
+const cancel = document.querySelector('.cancel');
+const read = document.querySelector('#read');
+const remove = document.querySelector('.remove')
+const readConfirm = document.querySelector('.readConfirm');
+const fillCard = document.querySelector('.fillCard');
+const popUp = document.querySelector('.popUp')
+
+
+addBtn.addEventListener('click', () => popUp.style.display = 'flex');
+cancel.addEventListener('click', () => popUp.style.display = 'none')
+
+
+// Constructor
+class Book {
+    constructor(title, author, pages, read) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
     }
 }
 
+// Create Library empty Array
+let myLibrary = []
+let addBook;
 
-
-function addBookToLibrary(book) {
-    myLibrary.push(book)
+//Take value of elements and return an object
+const addBookToLibrary = () => {
+    const title = document.getElementById('title').value
+    const author = document.getElementById('author').value
+    const pages = document.getElementById('pages').value
+    const read = document.getElementById('read').checked
+    return new Book(title, author, pages, read)
 }
 
-// display book on page
-// alternatively can use .forEach() ??
-for (i = 0; i < myLibrary.length; i++) {
+// Add objects to the array and render books on the page
+
+const update = (e) => {
+    e.preventDefault()
+    popUp.style.display = 'none';
+
+    const newBook = addBookToLibrary()
+    myLibrary.push(newBook);
+    resetBooks();
 }
 
 
-// const theHobbit = new Book('The Hobbit', 'J.R.R Tolkien', '295', 'read')
+const resetBooks = () => {
+    const display = document.querySelector('.libraryContainer');
+    const cards = document.querySelectorAll('.card');
+    cards.forEach(card => display.removeChild(card));
+    
+    for (let i=0; i<myLibrary.length; i++){
+        addCard(myLibrary[i]);
+    }
+}
+
+// DOM Elements 
+
+const addCard = (book) => {
+    const libraryDiv = document.querySelector('.libraryContainer')
+    const cardDiv = document.createElement('div');
+    const bookCardInfoDiv = document.createElement('div');
+    const titleDiv = document.createElement('div');
+    const authorDiv = document.createElement('div');
+    const pagesDiv = document.createElement('div');
+    const bookCardOperationsDiv = document.createElement('div');
+    const readConfirmBtn = document.createElement('button');
+    const removeBtn = document.createElement('button');
+
+    cardDiv.classList.add('card');
+    bookCardInfoDiv.classList.add('bookCardInfo');
+    titleDiv.classList.add('title');
+    authorDiv.classList.add('author');
+    pagesDiv.classList.add('pages');
+    bookCardOperationsDiv.classList.add('bookCardOperations');
+    readConfirmBtn.classList.add('readConfirm');
+    removeBtn.classList.add('remove');
+    
+    if(book.read===false) {
+        readConfirmBtn.classList.add('notRead');
+    }
+
+    readConfirmBtn.addEventListener('click', () => {
+        book.read = !book.read;
+        resetBooks();
+    })
+
+    removeBtn.addEventListener('click', () => {
+        myLibrary.splice(myLibrary.indexOf(book), 1)
+        resetBooks();
+    })
+
+    titleDiv.textContent = `${book.title}`
+    authorDiv.textContent = book.author
+    pagesDiv.textContent = `${book.pages} pages`
+    removeBtn.innerHTML = '<span class="iconify" data-icon="iconoir:cancel"></span>'
+    readConfirmBtn.innerHTML = '<span class="iconify" data-icon="ant-design:read-outlined">'
+
+    cardDiv.appendChild(bookCardInfoDiv);
+    bookCardInfoDiv.appendChild(titleDiv);
+    bookCardInfoDiv.appendChild(authorDiv);
+    bookCardInfoDiv.appendChild(pagesDiv);
+    cardDiv.appendChild(bookCardOperationsDiv)
+    bookCardOperationsDiv.appendChild(readConfirmBtn)
+    bookCardOperationsDiv.appendChild(removeBtn)
+    libraryDiv.appendChild(cardDiv);
+}
+
+submit.addEventListener('click', update)
+
+
